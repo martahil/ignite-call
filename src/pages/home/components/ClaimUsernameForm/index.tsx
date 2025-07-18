dbe91bd2c61098d0046ea5db82fb64c4a5f6f3e0
Marta Hilgert <marta.hil.1988@gmail.com>
2025-07-17 22:30:44 -0300

    feat: display form validation errors using zod and styled annotation

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,37 +1,58 @@
-import { Button, TextInput } from '@ignite-ui/react'
-import { Form } from './styles'
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { Form, FormAnnotation } from './styles'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight } from 'phosphor-react'
-import z from 'zod'
+import { z } from 'zod'
 
-const ClaimUsernameFormSchema = z.object({
-  username: z.string(),
+const claimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'Username must be at least 3 characters long.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'Username can only contain letters and hyphens.',
+    })
+    .transform((username) => username.toLowerCase())
 })
 
-type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
-  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { errors },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
 
   async function handleClaimUsername(data: ClaimUsernameFormData) {
     console.log(data)
   }
 
   return (
-    <Form as='form' onSubmit={handleSubmit(handleClaimUsername)}>
-      <TextInput 
-        size='sm'
-        prefix='ignite.com/'
-        placeholder='your-username'
-        {...register('username')}
-      />
-      <Button
-        size='sm'
-        type='submit'
-      >
-        Schedule
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as='form' onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size='sm'
+          prefix='ignite.com/'
+          placeholder='your-username'
+          {...register('username')}
+        />
+        <Button
+          size='sm'
+          type='submit'
+        >
+          Schedule
+          <ArrowRight />
+        </Button>
+      </Form>
+
+      <FormAnnotation>
+        <Text size='sm'>
+          {errors.username ? errors.username?.message : 'Enter the username you want'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
