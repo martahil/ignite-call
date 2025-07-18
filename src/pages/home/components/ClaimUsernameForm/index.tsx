a9b8d6f53eed53c4a2be5406641dc3154bef6ffb
Marta Hilgert <marta.hil.1988@gmail.com>
2025-07-17 21:50:22 -0300

    feat: implement form handling and validation with react-hook-form and zod

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,17 +1,32 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { Form } from './styles'
+import { useForm } from 'react-hook-form'
 import { ArrowRight } from 'phosphor-react'
+import z from 'zod'
+
+const ClaimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as='form'>
+    <Form as='form' onSubmit={handleSubmit(handleClaimUsername)}>
       <TextInput 
-        size={'sm'}
+        size='sm'
         prefix='ignite.com/'
         placeholder='your-username'
+        {...register('username')}
       />
       <Button
-        size={'sm'}
+        size='sm'
         type='submit'
       >
         Schedule
