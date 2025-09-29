98ee76afbdf112cde1092898b902ed5583fcbc74
Marta Hilgert <marta.hil.1988@gmail.com>
2025-09-28 21:48:20 -0300

    chore: disable eslint checks during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   pageExtensions: [
     'page.tsx',
@@ -13,4 +11,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
